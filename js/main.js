04837d02089b9765d0a16c8fd71b7261cfd6f600
seohyon-cho <flanique@naver.com>
2023-11-13 11:08:24 +0900

    tab 메뉴 함수 생성 심화

--- js/main.js
+++ js/main.js
@@ -2,11 +2,19 @@ const main = document.querySelector('main');
 const btns = main.querySelectorAll('li');
 const boxs = main.querySelectorAll('article');
 
-btns.forEach((el, idx) => {
-	el.addEventListener('click', () => {
-		btns.forEach((el) => el.classList.remove('on'));
-		btns[idx].classList.add('on');
-		boxs.forEach((el) => el.classList.remove('on'));
-		boxs[idx].classList.add('on');
+// 버튼 클릭 이벤트문
+btns.forEach((btn, idx) => {
+	btn.addEventListener('click', () => {
+		//activation(btns, idx);
+		//activation(boxs, idx);
+		[btns, boxs].forEach((el) => activation(el, idx));
 	});
 });
+
+// 인수로 그룹 선택자와, 순번을 받아서
+// 해당 순번에 해당하는 요소만 활성화시키는 함수
+
+function activation(arr, index) {
+	arr.forEach((el) => el.classList.remove('on'));
+	arr[index].classList.add('on');
+}
