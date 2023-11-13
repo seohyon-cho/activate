edfa031df39a6a069e107c5c773d932cd94933da
seohyon-cho <flanique@naver.com>
2023-11-13 11:00:42 +0900

    tab 메뉴 기능 기본 로직 완성 (2)

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ const main = document.querySelector('main');
 const btns = main.querySelectorAll('li');
 const boxs = main.querySelectorAll('article');
 
-btns.forEach((el, idx) => {
-	el.addEventListener('click', () => {
+btns.forEach((btn, idx) => {
+	btn.addEventListener('click', () => {
 		btns.forEach((el) => el.classList.remove('on'));
 		btns[idx].classList.add('on');
 		boxs.forEach((el) => el.classList.remove('on'));
