ad5efc76fad4a8626a6b00e63f71f435d578483c
seohyon-cho <flanique@naver.com>
2023-11-13 10:59:37 +0900

    tab 메뉴 기능 기본 로직 완성

--- js/main.js
+++ js/main.js
@@ -2,15 +2,11 @@ const main = document.querySelector('main');
 const btns = main.querySelectorAll('li');
 const boxs = main.querySelectorAll('article');
 
-btns.forEach((btn, idx) => {
-	btn.addEventListener('click', () => {
-		// 직전의 모든 요소 초기화
-		btns.forEach((btn) => btn.classList.remove('on'));
-		// 클릭한 순번에 해당하는 버튼만 활성화
+btns.forEach((el, idx) => {
+	el.addEventListener('click', () => {
+		btns.forEach((el) => el.classList.remove('on'));
 		btns[idx].classList.add('on');
-		// 박스 초기화
-		boxs.forEach((btn) => btn.classList.remove('on'));
-		// 클릭한 버튼에 해당하는 박스만 활성화
+		boxs.forEach((el) => el.classList.remove('on'));
 		boxs[idx].classList.add('on');
 	});
 });
