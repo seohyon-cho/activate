e64423302e20676e0d5eb59b96414eb6d7cb718b
seohyon-cho <flanique@naver.com>
2023-11-13 12:39:38 +0900

    DOM요소의 속성 변경

--- js/main.js
+++ js/main.js
@@ -1,29 +1,10 @@
-const main = document.querySelector('main');
-const btns = main.querySelectorAll('li');
-const boxs = main.querySelectorAll('article');
+// DOM 요소의 속성값 제어
 
-// 미션 : 아래 구문에서 e.currentTarget을 활용하여
-// 각각의 버튼 클릭 시, 클릭한 li의 글자값을 콘솔로 출력.
-btns.forEach((btn, idx) => {
-	btn.addEventListener('click', (e) => {
-		// 클릭 시 해당 li의 글자 확인.
-		console.log(e.currentTarget.innerText);
-		// 클릭한 해당 li의 글자값 (innerText) 변경.
-		// innerText는 e라는 객체의 key이기 때문에, 대입연산자로 값 변경가능.
-		// e.currentTarget.innerText = idx;
+// DOM.getAttribute('속성명'): 해당 속성명의 값 가져오기
+// DOM.setAttribute('속성명', '변경할 속성 값'): 해당 속성 명의 값을 변경
 
-		// 클릭한 해당 li의 글자값을 가져와서 <span>으로 감싼 다음에, li에 innerHTML로 등록.
-		e.currentTarget.innerHTML = `<span>${e.currentTarget.innerText}</span>`;
-		btns.forEach((el) => el.classList.remove('on'));
-		btns[idx].classList.add('on');
-		boxs.forEach((el) => el.classList.remove('on'));
-		boxs[idx].classList.add('on');
-	});
-});
-
-console.dir(btns[0]);
-/* DOM안에서 자주 쓰이는 유용한 property
-	- DOM.innerText : 해당 돔에 연결되어 있는 text값을 확인 가능. 
-	- DOM.innerHTML : 해당 돔에 연결되어 있는 자식 tag 구조를 확인 가능. 
-	- DOM.className : 해당 돔에 연결되어 있는 모든 클래스명을 문자열로 확인 가능. 
-*/
+const link = document.querySelector('a');
+console.log(link.getAttribute('href')); // a요소의 href 속성 값을 출력
+link.setAttribute('href', 'https://www.nate.com');
+link.setAttribute('target', '_blank');
+link.innerText = '네이트';
