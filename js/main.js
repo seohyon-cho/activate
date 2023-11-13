c7c35a49a11f85a280f48b64dd5b1001563e723e
seohyon-cho <flanique@naver.com>
2023-11-13 13:44:00 +0900

    innerHTML을 활용한 반복 요소 동적 생성 방법 (내가 해본 버전)

--- js/main.js
+++ js/main.js
@@ -18,25 +18,30 @@ const info = [
 
 /* 
   동적으로 DOM 생성하기
-
   방법 (1) : innerHTML을 이용해서 문자열로 직접 동적 태그 생성 (선택자의 기존 자식 내용을 없애면서 덮어쓰기)
-
   방법 (2)
-
 */
 
 // 특정 부모 요소 안에 반복적인 DOM 요소를 동적으로 생성하기 위해서는
 
+// const nav = document.querySelector('nav');
+// // 1. 문자열을 초기화 하는 변수를 만들고
+// let tags = '';
+// // 2. += 를 활용해서, 반복 추가할 태그를 문자열 형태로 계속해서 쌓아놓음.
+// tags += `<a href='${info[0].url}' target='${info[0].target}'>${info[0].name}</a>`;
+
+// tags += `<a href='${info[1].url}' target='${info[1].target}'>${info[1].name}</a>`;
+
+// tags += `<a href='${info[2].url}' target='${info[2].target}'>${info[2].name}</a>`;
+
+// console.log(tags);
+// // 3. 해당 DOM 요소에 변수에 있는(tags) 반복태그 문자열들을 한 번에 꽂아넣음.
+// nav.innerHTML = tags;
+
 const nav = document.querySelector('nav');
-// 1. 문자열을 초기화 하는 변수를 만들고
 let tags = '';
-// 2. += 를 활용해서, 반복 추가할 태그를 문자열 형태로 계속해서 쌓아놓음.
-tags += `<a href='${info[0].url}' target='${info[0].target}'>${info[0].name}</a>`;
-
-tags += `<a href='${info[1].url}' target='${info[1].target}'>${info[1].name}</a>`;
-
-tags += `<a href='${info[2].url}' target='${info[2].target}'>${info[2].name}</a>`;
-
+info.forEach((data, index) => {
+	tags += `<a href='${info[index].url}' target='${info[index].target}'>${info[index].name}</a>`;
+});
 console.log(tags);
-// 3. 해당 DOM 요소에 변수에 있는(tags) 반복태그 문자열들을 한 번에 꽂아넣음.
 nav.innerHTML = tags;
